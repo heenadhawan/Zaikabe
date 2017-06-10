1df3816f2092a561b458b0069d76b5e972f824db
Heena <Heena@DESKTOP-IV4RLKR>
2017-06-10 13:01:48 -0700

    integrate the project

---
+++ src/main/java/com/niit/dao/CategoryDao.java
@@ -0,0 +1,9 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import com.niit.dto.Category;
+
+public interface CategoryDao {
+List<Category> list();
+}
---
+++ src/main/java/com/niit/daoimpl/CategoryDaoImpl.java
@@ -0,0 +1,17 @@
+package com.niit.daoimpl;
+
+import java.util.List;
+
+import org. springframework.stereotype.Repository;
+
+import com.niit.dao.CategoryDao;
+import com.niit.dto.Category;
+@Repository("categoryDao")
+public class CategoryDaoImpl implements CategoryDao {
+
+	public List<Category> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
